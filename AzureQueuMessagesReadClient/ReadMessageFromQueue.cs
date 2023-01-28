81e87a3c7deb0c8667132c476217d0896af611eb
durgesh-sharma <durgesh.net4@gmail.com>
2023-01-28 22:23:29 +0530

    Queue read and write
    Initial Commit

---
+++ AzureQueuMessagesReadClient/ReadMessageFromQueue.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
+
+internal class ReadMessageFromQueue
+{
+    static QueueClient queueClient;
+    private static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+                                    .SetBasePath(Directory.GetCurrentDirectory())
+                                    .AddJsonFile("appsettings.json");
+        IConfiguration config = builder.Build();
+        string connectionString = config["StorageConnectionString"];
+        string queueName = "demoqueue";
+        queueClient = new QueueClient(connectionString, queueName);
+        queueClient.CreateIfNotExists();
+
+        while (true)
+        {
+            QueueMessage[] retrievedMessages = queueClient.ReceiveMessages(1); //Fetches only one message from queue (visibilitytimeout=30)
+            if (retrievedMessages.Length == 0)
+                Console.WriteLine("No Messages in last 5 secs...");
+            foreach (var msg in retrievedMessages)
+            {
+                Console.WriteLine($"Dequeued message: '{msg.Body}'");
+                queueClient.DeleteMessage(msg.MessageId, msg.PopReceipt);
+            }
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+
+}
